dba47ba0371ec5601593f61a2794cf3144cfbb74
INGRID HECK <ingridlheck@gmail.com>
2022-11-30 13:08:12 +0100

    : Small design changes

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -7,26 +7,30 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Welcome to spotiQuiz!</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#000',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#1DB954',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+    color: '#1DB954',
   },
 });
