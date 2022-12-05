9276053c4f71ef2702fe3f3a519e7f4a7faffe02
INGRID HECK <ingridlheck@gmail.com>
2022-12-05 14:33:29 +0100

    edit start screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,70 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button, Alert, SafeAreaView, Pressable } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  let [count, setCount] = useState(0);
+
+  const Separator = () => (
+    <View style={styles.separator} />
+  );
+
+
   return (
+    <SafeAreaView style = {styles.container}>
+<View>
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to spotiQuiz!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
+      <View>
+      <Button
+        title="Play"
+        color="#1DB954"
+        onPress={() => Alert.alert('Simple Button pressed') }
+      />
+      </View>
+
+      <View>
+      <Button
+        title="Scoreboard"
+        color="#1DB954"
+        onPress={() => Alert.alert('Simple Button pressed') }
+      />
+      </View>
+      <View>
+      <Button
+        title="Statistics"
+        color="#1DB954"
+        onPress={() => Alert.alert('Simple Button pressed') }
+      />
+      </View>
+      <View>
+      <Button
+        title="Settings"
+        color="#1DB954"
+        onPress={() => Alert.alert('Simple Button pressed') }
+      />
+      </View>
+      <View>
+    <Button
+        title="Log out "
+        color="#D9D9D9"
+        onPress={() => Alert.alert('Simple Button pressed') }
+      />
+      </View>
+      </View>
+
+  
+
+
+
+    </SafeAreaView>
+
+    
   );
 }
 
