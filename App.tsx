52d2fd5b1c7d4c7e321730bc87de39187e5e5563
André Schjøth <aschjoth@gmail.com>
2023-01-06 18:34:17 +0100

    chore: remove unused code

--- App.tsx
+++ App.tsx
@@ -1,36 +1,32 @@
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import  HomeScreen from './screens/HomeScreen';
-import ModalScreen from './screens/ModalScreen';
-import Scoreboard from './screens/Scoreboard';
-import Settings from './screens/Settings';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import ModalScreen from "./screens/ModalScreen";
+import Scoreboard from "./screens/Scoreboard";
+import Settings from "./screens/Settings";
 
 type RootStackParamList = {
-  Home: undefined, // undefined because you aren't passing any params to the home screen
-  Modal: undefined, //Issue Is here i Think
-  Scoreboard: undefined,
-  Settings: undefined,
-  Profile: { name: string }; 
+	Home: undefined; // undefined because you aren't passing any params to the home screen
+	Modal: undefined; //Issue Is here i Think
+	Scoreboard: undefined;
+	Settings: undefined;
+	Profile: { name: string };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-
-
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} /> 
-        <Stack.Screen name="Modal" component={ModalScreen} /> 
-        <Stack.Screen name="Scoreboard" component={Scoreboard} />
-        <Stack.Screen name="Settings" component={Settings}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Modal" component={ModalScreen} />
+				<Stack.Screen name="Scoreboard" component={Scoreboard} />
+				<Stack.Screen name="Settings" component={Settings} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 export default App;
